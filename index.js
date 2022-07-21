c67646413402963e224da533db0445ed018b0800
jarifrahman <jarifrahman@zoho.com>
2022-07-21 18:43:48 -0400

    second

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const util= require('util');
+const encoder = new util.TextEncoder('utf-8');
 const { MongoClient } = require("mongodb");
 app.use(express.urlencoded({ extended: true }));
 
